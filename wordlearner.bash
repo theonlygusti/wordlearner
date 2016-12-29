67394ab8be8e286400809fc9d3f4b376685c1057
BuildTools <theonlygusti@gmail.com>
2016-12-29 09:00:44 +0000

    Fixed #1 by using different voices for each target language

--- wordlearner.bash
+++ wordlearner.bash
@@ -1,6 +1,7 @@
 correct=0
 total=0
 source languages.cfg
+source voices.cfg
 while read line
 do
   let "total=$total+1"
@@ -8,7 +9,7 @@ do
   ./translate-shell/translate "$learning":"$native" -brief "$line"
   read -r answer < /dev/tty
   echo -en "\e[2A"; echo -e "\e[0K\r$total $line"
-  say -v Jorge "$line"
+  say -v "${!learning}" "$line"
   if [ "$answer" == "$line" ]; then
     let "correct=$correct+1"
     echo -en "\e[1A"; echo -e "\e[0K\rcorrect"
