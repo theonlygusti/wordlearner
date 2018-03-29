2018e353f6f47b0d13d92388a9d7671629a5afff
George <theonlygusti@gmail.com>
2018-03-29 10:55:59 +0100

    "

--- wordlearner.bash
+++ wordlearner.bash
@@ -5,16 +5,15 @@ source voices.cfg
 while read line
 do
   let "total=$total+1"
-  echo -en "\e[0K\r$total "
+  echo -en "$total "
   ./translate-shell/translate "$learning":"$native" -brief "$line"
   read -r answer < /dev/tty
-  echo -en "\e[2A"; echo -e "\e[0K\r$total $line"
+  echo -e "$total $line"
   say -v "${!learning}" "$line"
   if [ "$answer" == "$line" ]; then
     let "correct=$correct+1"
   fi
-  echo -en "\e[1A"; [ "$answer" == "$line" ] &&  echo -e "\e[0K\r$total $answer ✓" || echo -e "\e[0K\r$total $answer ✗"
+  [ "$answer" == "$line" ] &&  echo -e "$total $answer ✓" || echo -e "$total $answer ✗"
 done
 
-echo -e "\e[0K\r"
 echo -e "\nscore: $correct / $total"
